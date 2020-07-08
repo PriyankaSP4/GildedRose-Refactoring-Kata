50d6b1526bf8f4a0fbf3fb36d8dd09f73d97a530
PriyankaSP4 <67911205+PriyankaSP4@users.noreply.github.com>
2020-07-08 12:25:25 +0100

    Update GildedRoseTest.kt

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -5,20 +5,34 @@ import org.junit.Test
 
 class GildedRoseTest {
 
-    @Test fun normal_item_behaviour() {
+    @Test fun normal_item_name_stays_same() {
         //test name, decrement of sellIn and quality for normal item
         val items = arrayOf<Item>(Item("fool", 1, 10))
         val app = GildedRose(items)
         app.updateQuality()
         assertEquals("fool", app.items[0].name)
+    }
+
+    @Test fun normal_item_sellIn_decrement() {
+        //test name, decrement of sellIn and quality for normal item
+        val items = arrayOf<Item>(Item("fool", 1, 10))
+        val app = GildedRose(items)
+        app.updateQuality()
         assertEquals(0, app.items[0].sellIn)
+    }
+
+    @Test fun normal_item_quality_decrement_before_sellIn_date_passes() {
+        //test name, decrement of sellIn and quality for normal item
+        val items = arrayOf<Item>(Item("fool", 1, 10))
+        val app = GildedRose(items)
+        app.updateQuality()
         assertEquals(9, app.items[0].quality)
     }
 
-    @Test fun normal_item_decrementing_quality() {
+    @Test fun normal_item_decrementing_quality_after_sellIn_date_passes() {
+        //test double degradation past sellIn
         val items = arrayOf<Item>(Item("fool", 0, 9))
         val app = GildedRose(items)
-        //test double degradation past sellIn
         app.updateQuality()
         assertEquals(-1, app.items[0].sellIn)
         assertEquals(7, app.items[0].quality)
@@ -40,17 +54,23 @@ class GildedRoseTest {
         assertEquals(0, app.items[0].quality)
     }
 
-    @Test fun sulfuras_behaviour() {
+    @Test fun sulfuras_unchanged_quality() {
+        val items = arrayOf<Item>(Item("Sulfuras, Hand of Ragnaros", 30, 30), Item("Sulfuras, Hand of Ragnaros", 0, 30))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(30, app.items[0].quality)
+        assertEquals(30, app.items[1].quality)
+    }
+
+    @Test fun sulfuras_unchanged_sellIn() {
         val items = arrayOf<Item>(Item("Sulfuras, Hand of Ragnaros", 30, 30), Item("Sulfuras, Hand of Ragnaros", 0, 30))
         val app = GildedRose(items)
         app.updateQuality()
         assertEquals(30, app.items[0].sellIn)
-        assertEquals(30, app.items[0].quality)
         assertEquals(0, app.items[1].sellIn)
-        assertEquals(30, app.items[1].quality)
     }
 
-    @Test fun brie_behaviour() {
+    @Test fun brie_increasing_in_quality() {
         val items = arrayOf<Item>(Item("Aged Brie", 10, 30))
         val app = GildedRose(items)
         app.updateQuality()
@@ -58,17 +78,21 @@ class GildedRoseTest {
         assertEquals(31, app.items[0].quality)
     }
 
-    @Test fun backstage_passes_behaviour() {
+    @Test fun backstage_passes_quality_rules_dep_on_sellIn() {
         val items = arrayOf<Item>(Item("Backstage passes to a TAFKAL80ETC concert", 20, 20), Item("Backstage passes to a TAFKAL80ETC concert", 10, 30), Item("Backstage passes to a TAFKAL80ETC concert", 5, 40), Item("Backstage passes to a TAFKAL80ETC concert", 0, 50))
         val app = GildedRose(items)
         app.updateQuality()
+        //sellIn > 10, quality increments by 1
         assertEquals(21, app.items[0].quality)
+        //6 < sellIn < 10, quality increments by 2
         assertEquals(32, app.items[1].quality)
+        // sellIn < 6, quality increments by 3
         assertEquals(43, app.items[2].quality)
+        //sellIn < 0, quality drops to 0
         assertEquals(0, app.items[3].quality)
     }
 
-    @Test fun conjured_normal_item_behaviour() {
+    @Test fun conjured_normal_item_degrading_twice_as_fast() {
         val items = arrayOf<Item>(Item("Conjured fool", 5, 30), Item("Conjured fool", 0, 10))
         val app = GildedRose(items)
         app.updateQuality()
@@ -76,7 +100,7 @@ class GildedRoseTest {
         assertEquals(6, app.items[1].quality)
     }
 
-    @Test fun conjured_sulfuras_behaviour() {
+    @Test fun conjured_sulfuras_quality_still_staying_same() {
         val items = arrayOf<Item>(Item("Conjured Sulfuras, Hand of Ragnaros", 5, 30))
         val app = GildedRose(items)
         app.updateQuality()
