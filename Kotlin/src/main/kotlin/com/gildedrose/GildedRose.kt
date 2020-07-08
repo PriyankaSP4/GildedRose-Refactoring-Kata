59baabb4fb7ea860551db0362c2c61e0688cc79d
emmacbragg <67954195+emmacbragg@users.noreply.github.com>
2020-07-08 12:45:28 +0100

    Update GildedRose.kt

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,16 +1,14 @@
 package com.gildedrose
 
 class GildedRose(var items: Array<Item>) {
-    
-    fun qualityDec(quality: Int, name: String, sulfuras:String, conjured:String) : Int{
-        var qual = quality
-        if (qual > 0 && !name.contains(sulfuras)) {
-            qual -= 1
-            if (name.contains(conjured)) {
-                qual -= 1
-            }
+
+    fun qualityDec(quality: Int) : Int{
+        if (quality > 0){
+            return quality - 1
         }
-        return qual
+        else{
+            return quality
+        }
     }
 
     fun qualityInc(quality:Int): Int{
@@ -20,55 +18,81 @@ class GildedRose(var items: Array<Item>) {
         else{
             return quality
         }
+    }
 
+    fun sellInDec(sellIn: Int) : Int{
+        return sellIn - 1
+    }
+
+    fun normal(i:Int) {
+        items[i].quality = qualityDec(items[i].quality)
+        items[i].sellIn = sellInDec(items[i].sellIn)
+        if (items[i].sellIn < 0) {
+            items[i].quality = qualityDec(items[i].quality)
+        }
+    }
+
+    fun brie(i:Int){
+        items[i].quality = qualityInc(items[i].quality)
+        items[i].sellIn = sellInDec(items[i].sellIn)
+    }
+
+    fun backstage(i:Int){
+        items[i].quality = qualityInc(items[i].quality)
+
+        if (items[i].sellIn < 11) {
+            items[i].quality = qualityInc(items[i].quality)
+        }
+        if (items[i].sellIn < 6) {
+            items[i].quality = qualityInc(items[i].quality)
+        }
+
+        if (items[i].sellIn < 1) {
+            items[i].quality = 0
+        }
+    }
+
+    fun conjured(i:Int){
+        items[i].quality = qualityDec(items[i].quality)
+        items[i].quality = qualityDec(items[i].quality)
+        items[i].sellIn = sellInDec(items[i].sellIn)
+        if (items[i].sellIn < 0) {
+            items[i].quality = qualityDec(items[i].quality)
+            items[i].quality = qualityDec(items[i].quality)
+        }
+    }
+
+    fun maxQual(i:Int){
+        if (items[i].quality > 50) {
+            items[i].quality = 50
+        }
     }
 
     fun updateQuality() {
         for (i in items.indices) {
             val name = items[i].name
-            var quality = items[i].quality
-            var sellIn = items[i].sellIn
             var backstage = "Backstage passes"
             var sulfuras = "Sulfuras"
             var brie = "Aged Brie"
             var conjured = "Conjured"
 
-            if (quality > 50) {
-                quality = 50
-            }
+            maxQual(i)
 
-            if (name != brie && !name.contains(backstage)) {
-                quality = qualityDec(quality, name, sulfuras, conjured)
-
-            } else {
-                quality = qualityInc(quality)
-                if (name.contains(backstage)) {
-                    if (sellIn < 11) {
-                        quality = qualityInc(quality)
-                    }
-                    if (sellIn < 6) {
-                        quality = qualityInc(quality)
-                    }
-                }
+            if (name == brie){
+                brie(i)
             }
-
-            if (!name.contains(sulfuras)) {
-                sellIn -= 1
+            else if (name.contains(backstage)){
+                backstage(i)
             }
-
-            if (sellIn < 0) {
-                if (name != brie) {
-                    if (!name.contains(backstage)) {
-                        quality = qualityDec(quality, name, sulfuras, conjured)
-                    } else {
-                        quality = 0
-                    }
-                } else {
-                    quality = qualityInc(quality)
-                }
+            else if (name.contains(sulfuras)){
+                break
             }
-            items[i].quality = quality
-            items[i].sellIn = sellIn
+            else if (name.contains(conjured)){
+                conjured(i)
+            }
+            else{
+                normal(i)
+            }
         }
     }
 }
